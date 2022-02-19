1e5fca2f5405c1d33ce7577a2c016e7bf131d38c
Asad89 <usmonovasad89@gmail.com>
2022-02-19 14:04:16 +0300

    fixed bugs commited for deploy

--- components/Product.js
+++ components/Product.js
@@ -4,7 +4,7 @@ import Currency from "react-currency-formatter";
 
 export default function Product({hasPrime, title,description,image,category,rating,price}) {
   const ratingNum = Math.floor(rating.rate);
-console.log(hasPrime)
+
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
         <p className="absolute top-2 right-2 italic text-xs text-gray-400">{category}</p>
--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -1,8 +1,6 @@
-
 import Product from "./Product";
 
 export default function ProductFeed({ products }) {
-  console.log(products)
   return (
     <div className="grid grid-flow-row-dense md:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
       {products
